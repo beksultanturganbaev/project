8d11c9303d8d5cab86f12d800e58aeca7df55f6a
Beka <example@gmail.com>
2021-04-10 14:16:11 +0600

    second domashnye zadanya

--- script.js
+++ script.js
@@ -11,14 +11,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const movies = [];
+for (let i = 0; i < 2; i++){
+    const a = prompt('Odin iz poslednih prosmotrennyh filmov?', ''),
+          b = prompt('Na skolko otsenite ego?', '');
 
-const a = prompt('Odin iz poslednih prosmotrennyh filmov?', ''),
-      b = prompt('Na skolko otsenite ego?', ''),
-      c = prompt('Odin iz poslednih prosmotrennyh filmov?', ''),
-      d = prompt('Na skolko otsenite ego?', '');
+    if (a != null && b != null && a != '' && b !='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else{
+        console.log('Error');
+        i--; 
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log('posmotreno dovolno malo filmov');
+} else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+    console.log('vy kalsichiskii zritel');
+} else if (personalMovieDB.count >=30) {
+    console.log('vy kinoman');
+} else{
+    console.log('prizawlo owibka');
+}
 
 console.log(personalMovieDB);
