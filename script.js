922f97193b7d37a20d7db79998b6953d34004fd6
Beka <example@gmail.com>
2021-04-10 22:34:21 +0600

    third domashnye zadanya

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
 "use strict";
 
-const numberOfFilms = +prompt('Skolko filmov vy uje posmotreli?', '');
+let numberOfFilms;
 
+function start(){
+    numberOfFilms = prompt('Skolko filmov vy uje posmotreli?', '');
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = prompt('Skolko filmov vy uje posmotreli?', '');
+    }
+}
+start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: numberOfFilms, 
     movies: {}, 
     actors: {},
-    genres: {},
+    genres: [],
     privat: false
 };
 
-for (let i = 0; i < 2; i++){
-    const a = prompt('Odin iz poslednih prosmotrennyh filmov?', ''),
-          b = prompt('Na skolko otsenite ego?', '');
-
-    if (a != null && b != null && a != '' && b !='' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else{
-        console.log('Error');
-        i--; 
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++){
+        const a = prompt('Odin iz poslednih prosmotrennyh filmov?', ''),
+              b = prompt('Na skolko otsenite ego?', '');
+    
+        if (a != null && b != null && a != '' && b !='' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else{
+            console.log('Error');
+            i--; 
+        }
     }
 }
+//rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    console.log('posmotreno dovolno malo filmov');
-} else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
-    console.log('vy kalsichiskii zritel');
-} else if (personalMovieDB.count >=30) {
-    console.log('vy kinoman');
-} else{
-    console.log('prizawlo owibka');
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10) {
+        console.log('posmotreno dovolno malo filmov');
+    } else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+        console.log('vy kalsichiskii zritel');
+    } else if (personalMovieDB.count >=30) {
+        console.log('vy kinoman');
+    } else{
+        console.log('prizawlo owibka');
+    }
 }
+//detectPersonalLevel();
 
-console.log(personalMovieDB);
+function showMyDB(hidden){
+    if (!hidden){
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i-1] = prompt(`vash liubimyi janr ${i}`);
+    }
+}
+writeYourGenres();
